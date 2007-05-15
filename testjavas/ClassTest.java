ae7e2546c8fe6fe1282bf352081fe43b1fd41420
crow.phil <crow.phil@051a13bb-8230-0410-845a-a3e37d5735c0>
2007-05-15 23:07:29 +0000

    Added java test code.  It's needed instead of SDK code to avoid new methods.

---
+++ testjavas/ClassTest.java
@@ -0,0 +1,7 @@
+public class ClassTest {
+    public ClassTest() {}
+    public ClassTest( java.lang.String[] greetings ) {}
+    public ClassTest( java.lang.String[] greetings, int default_val ) {}
+    public java.lang.String getGreet() { return ""; }
+    public java.lang.String getGreet(int number) { return ""; }
+}
---
+++ testjavas/IntTest.java
@@ -0,0 +1,11 @@
+public interface IntTest {
+    public static final int USELESS_CONSTANT = 4;
+    public abstract int[][] array_returner(
+        java.lang.String s, int i, float[] f, float[][] g
+    );
+
+    public abstract java.lang.String object_returner()
+        throws java.lang.NumberFormatException, java.lang.Exception;
+    public abstract java.lang.String object_returner(java.lang.String s)
+        throws java.lang.NumberFormatException;
+}
