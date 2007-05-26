6dc5fc6a1ac7347b707b85d0685524e2c6eb2676
crow.phil <crow.phil@051a13bb-8230-0410-845a-a3e37d5735c0>
2007-05-26 15:29:33 +0000

    Added emitter.

---
+++ lib/Java/Javap/Emit.pm
@@ -0,0 +1,34 @@
+package Java::Javap::Emit;
+use strict; use warnings;
+
+use base 'Exporter';
+
+our @EXPORT_OK = qw( emit );
+
+use Template;
+
+sub emit {
+    my $class_file = shift;
+    my $ast        = shift;
+    my $template   = shift;
+
+    my $tt       = Template->new( {
+        INCLUDE_PATH => 'templates',
+        POST_CHOMP   => 1,
+    } );
+
+    my $tt_vars = {
+        ast        => $ast,
+        gen_time   => scalar localtime(),
+        version    => '0.1',
+        class_file => $class_file,
+        command_line_flags => [
+            '--classpath testjavas',
+        ],
+    };
+
+    my $retval;
+    $tt->process( $template, $tt_vars, \$retval ) || die $tt->error();
+
+    return $retval;
+}
