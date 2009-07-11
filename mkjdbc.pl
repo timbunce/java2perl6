dbd35f50f74b96d5f5249f9a3f19c229caf9deee
tim.bunce <tim.bunce@051a13bb-8230-0410-845a-a3e37d5735c0>
2009-07-11 00:23:34 +0000

    Updated mkjdbc.pl to set PERL6LIB env var so the validation of the generated files works now (though many have errors).

--- mkjdbc.pl
+++ mkjdbc.pl
@@ -3,19 +3,23 @@
 use strict;
 use warnings;
 use autodie;
+use Cwd qw(getcwd);
 
-my $perl6 = "$ENV{HOME}/perl6/rakudo/perl6";
+my $outdir = 'jdbclib';
+
+my $perl6 = shift || "$ENV{HOME}/perl6/rakudo/perl6";
+$ENV{PERL6LIB} .= ":$outdir";
 
 system qq{make};
 
-system qq{rm -rf jdbclib.prev; mv -f jdbclib jdbclib.prev};
+system qq{rm -rf $outdir.prev; mv -f $outdir $outdir.prev};
 
-system qq{perl -Mblib blib/script/java2perl6 --outdir jdbclib --recurse --nest `cat jdbc_classes.txt`};
+system qq{perl -Mblib blib/script/java2perl6 --outdir $outdir --recurse --nest `cat jdbc_classes.txt`};
 
-system qq{diff -r -u jdbclib.prev jdbclib} if -d "jdbclib.prev";
+system qq{diff -r -u $outdir.prev $outdir} if -d "$outdir.prev";
 
 # would be nice to validate these from the 'bottom up'
 # XXX could build this into java2perl6 as an option
 # but java2perl6 need to be restructured to actually recurse
-system qq{find jdbclib -name \*.pm | xargs -L 1 -t $perl6};
+system qq{find $outdir -name \*.pm | xargs -L 1 -t $perl6};
 
