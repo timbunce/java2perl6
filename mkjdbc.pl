65fd56df72d63993bb6efe5d77e63f89f6b40534
tim.bunce <tim.bunce@051a13bb-8230-0410-845a-a3e37d5735c0>
2009-08-04 22:34:56 +0000

    java2perl6 now recurses to handle referenced types. Added --check option to run perl6 -c on generated file as recursion unwinds. Updated mkjdbc.pl to use --check option.

--- mkjdbc.pl
+++ mkjdbc.pl
@@ -14,12 +14,7 @@ system qq{make};
 
 system qq{rm -rf $outdir.prev; mv -f $outdir $outdir.prev};
 
-system qq{perl -Mblib blib/script/java2perl6 --outdir $outdir --recurse --nest `cat jdbc_classes.txt`};
+system qq{perl -Mblib blib/script/java2perl6 --outdir $outdir --recurse --nest --check `cat jdbc_classes.txt`};
 
 system qq{diff -r -u $outdir.prev $outdir} if -d "$outdir.prev";
-
-# would be nice to validate these from the 'bottom up'
-# XXX could build this into java2perl6 as an option
-# but java2perl6 need to be restructured to actually recurse
-system qq{find $outdir -name \*.pm | xargs -L 1 -t $perl6};
 
