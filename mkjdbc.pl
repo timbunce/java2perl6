fa08df2e8c921e5e1ccfb3f281e53d854784a83e
tim.bunce <tim.bunce@051a13bb-8230-0410-845a-a3e37d5735c0>
2009-08-11 13:11:40 +0000

    Added :all to use autodie so system() gets checked. Added time to java2perl6 call. Added note to TODO.

--- mkjdbc.pl
+++ mkjdbc.pl
@@ -4,7 +4,7 @@
 
 use strict;
 use warnings;
-use autodie;
+use autodie qw(:all);
 use Cwd qw(getcwd);
 
 my $outdir = 'jdbclib';
@@ -15,7 +15,7 @@ system qq{make};
 
 system qq{rm -rf $outdir.prev; mv -f $outdir $outdir.prev};
 
-system qq{perl -Mblib blib/script/java2perl6 --outdir $outdir --recurse --nest --check `cat jdbc_classes.txt`};
+system qq{time perl -Mblib blib/script/java2perl6 --outdir $outdir --recurse --nest --check @ARGV `cat jdbc_classes.txt`};
 
 system qq{diff -r -u $outdir.prev $outdir} if -d "$outdir.prev";
 
