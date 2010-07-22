84668e8fb098e3c9e45b2abc4016bcfbf24312e4
Tim Bunce <Tim.Bunce@pobox.com>
2010-07-22 15:48:09 -0700

    just refer to java.sql.DriverManager for now as that pulls in the others

--- jdbclib-update.pl
+++ jdbclib-update.pl
@@ -32,7 +32,7 @@ my $outdir = 'jdbclib';
 system qq{rm -rf $outdir.prev} if -d "$outdir.prev";
 system qq{mv -f $outdir $outdir.prev} if -d $outdir;
 
-system qq{time perl -Mlib=lib bin/java2perl6api --add_types jdbclib-typemap --outdir $outdir @ARGV `cat jdbclib-classes.txt`};
+system qq{time perl -Mlib=lib bin/java2perl6api --add_types jdbclib-typemap --outdir $outdir @ARGV java.sql.DriverManager};
 
 system qq{diff -wr --exclude=*.pir -u $outdir.prev $outdir} || warn "Output differs\n"
     if -d "$outdir.prev";
