47679d345b6bf5e5ed9b8e9fe0d098a6ef2f9c23
Tim Bunce <Tim.Bunce@pobox.com>
2010-07-24 12:07:55 -0700

    need to ensure the native postgres libpq is loadable

--- dbdi-init.sh
+++ dbdi-init.sh
@@ -1,2 +1,7 @@
 # source into your shell
 export PERL6LIB=".:jdbclib:$PERL6LIB"
+
+# Also need to ensure the native postgres libpq is loadable
+# For OS X you'll need something like this in your ~/.profile
+# export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/usr/local/pgsql/lib/
+
